df948e6a369a8ce36edb0d586bc1a96a5ce36fe8
olvyko <olvyko@gmail.com>
2019-07-04 22:28:41 +0200

    Basic structure

---
+++ engine/src/engine.cpp
@@ -0,0 +1,7 @@
+#include <math.h>
+#include "engine.h"
+
+double calcSqrt(double d)
+{
+    return sqrt(d);
+}
---
+++ engine/src/engine.h
@@ -0,0 +1,6 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+double calcSqrt(double);
+
+#endif
